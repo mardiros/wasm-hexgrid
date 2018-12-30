6ddf8108da7e50addfab8c614a967b7b78b95022
Guillaume Gauvrit <guillaume@gauvr.it>
2018-12-30 11:07:26 +0100

    Fix constants

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,90 @@
-
 #[macro_use]
 extern crate log;
 
-use std::rc::Rc;
 use std::cell::RefCell;
 use std::f64::consts::PI;
+use std::rc::Rc;
 
-use stdweb::unstable::TryInto;
 use stdweb::traits::*;
+use stdweb::unstable::TryInto;
 
 use stdweb::web::html_element::CanvasElement;
 use stdweb::web::{document, CanvasRenderingContext2d, EventListenerHandle, FillRule};
 
 use stdweb::web::event::{ClickEvent, ConcreteEvent};
 
+pub const ANGLE: f64 = PI / 6.;
 
-pub const BOARD_SIZE: usize = 8;
-pub const TILE_SIZE: f64 = 24.;
-pub const BOARD_WIDTH: f64 = TILE_SIZE * 10.;
+pub const BOARD_SIDE: usize = 7;
+pub const BOARD_SIZE: usize = (BOARD_SIDE * 2) - 1;
+pub const TILE_SIZE: f64 = 32.;
+pub const BOARD_WIDTH: f64 = TILE_SIZE * (BOARD_SIZE as f64);
 
+pub fn tile_x() -> f64 {
+    TILE_SIZE * ANGLE.cos()
+}
+
+pub fn tile_y() -> f64 {
+    TILE_SIZE * ANGLE.sin()
+}
+
+pub fn board_width() -> f64 {
+    tile_x() * BOARD_SIZE as f64
+}
+
+pub fn margin() -> f64 {
+    tile_x()
+}
 
 pub struct HexTile {
     q: i32,
-    r: i32
+    r: i32,
 }
 
-
 impl HexTile {
-
     pub fn new(q: i32, r: i32) -> Self {
-        HexTile {q, r}
+        HexTile { q, r }
     }
-    
+
     fn coord_to_pos(&self) -> (f64, f64) {
-        let angle :f64 = PI / 6.;
-        let q = self.q as f64 * TILE_SIZE * angle.cos();
-        let r = self.r as f64 * TILE_SIZE * angle.cos();
-        let x = 2.*q + r;
-        let y = 2.* r * angle.cos();
+        let q = self.q as f64 * tile_x();
+        let r = self.r as f64 * tile_x();
+        let x = 2. * q + r;
+        let y = 2. * r * ANGLE.cos();
         (x, y)
     }
 
     pub fn paint(&self, context: &CanvasRenderingContext2d) {
         info!("Paint");
-        let delta :f64 = PI / 3.;
-        let mut angle :f64 = PI / 6.;
+        let delta: f64 = PI / 3.;
+        let mut angle: f64 = PI / 6.;
         let (x, y) = self.coord_to_pos();
         context.begin_path();
         context.move_to(
-            BOARD_WIDTH + x + TILE_SIZE * angle.cos(),
-            BOARD_WIDTH + y + TILE_SIZE * angle.sin(),
-            );
+            margin() + board_width() + x + tile_x(),
+            margin() + board_width() + y + TILE_SIZE * angle.sin(),
+        );
         for _ in 0..6 {
             angle += delta;
             context.line_to(
-                BOARD_WIDTH + x + TILE_SIZE * angle.cos(),
-                BOARD_WIDTH + y + TILE_SIZE * angle.sin(),
-                );
+                margin() + board_width() + x + TILE_SIZE * angle.cos(),
+                margin() + board_width() + y + TILE_SIZE * angle.sin(),
+            );
         }
+        context.set_fill_style_color("#333");
+        context.fill(FillRule::NonZero);
         context.stroke();
 
-        let text = format!("{} {}", self.q, self.r);
-        context.set_fill_style_color("#333");
-        context.fill_text(text.as_str(), BOARD_WIDTH + x - 5., BOARD_WIDTH + y, None);
+        let text = format!("{}   {}", self.q, self.r);
+        context.set_fill_style_color("#eee");
+        context.fill_text(
+            text.as_str(),
+            margin() + board_width() + x - 12.,
+            margin() + board_width() + y,
+            None,
+        );
     }
 }
-
 
 pub struct Store {
     game_over: bool,
@@ -87,16 +106,17 @@ impl Store {
     fn paint(&self, context: &CanvasRenderingContext2d) {
         info!("Paint");
 
-        context.set_stroke_style_color("#a24");  //red
-        for x in -5i32..6i32 {
-            for y in -5i32..6i32 {
-                if (x + y < 6 ) && (x + y > -6) {
+        let maxq: i32 = ((BOARD_SIZE as f32 / 2.).ceil()) as i32;
+        let minq: i32 = maxq - BOARD_SIZE as i32;
+        context.set_stroke_style_color("#a24"); //red
+        for x in minq..maxq {
+            for y in minq..maxq {
+                if (x + y < maxq) && (x + y > -maxq) {
                     let tile = HexTile::new(x, y);
                     tile.paint(context);
                 }
             }
-        } 
-
+        }
     }
 
     fn play(&mut self, x: usize, y: usize) -> Result<(), ()> {
@@ -117,7 +137,7 @@ impl Canvas {
             .try_into()
             .unwrap();
 
-        let canvas_width = store.cell_width() as u32 * BOARD_SIZE as u32;
+        let canvas_width = board_width() as u32 * 2 + 2 * margin() as u32;
 
         canvas.set_width(canvas_width);
         canvas.set_height(canvas_width);
